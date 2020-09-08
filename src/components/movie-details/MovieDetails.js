365173e81daa4b08c68e4199590eef0a2a3db4ee
eddy62 <eddyszandzik@gmail.com>
2020-09-08 09:06:18 +0200

    [#details film] [feat] [ES] [ajout des details d'un film]

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -3,7 +3,16 @@ import React, { Component } from "react";
 export default class MovieDetails extends Component {
 
     render() {
-        return <div className="w-25 border p-4"> Movie Details </div>
+        return (
+            <div className="w-25 border p-4 d-flex flex-column">
+                <h5>{ this.props.movie.title }</h5>
+                <hr className="w-100" />
+                <img className="d-block m-x" src={ this.props.movie.img} />
+                <p className="text-secondary">{ this.props.movie.details }</p>
+                <hr className="w-100" />
+                <p>{ this.props.movie.description }</p>
+            </div>
+        );
     }
 
 }
